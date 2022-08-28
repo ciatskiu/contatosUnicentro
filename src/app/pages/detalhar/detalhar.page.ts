67f3b099999fcbb1dd88d422216b2d85c9208d10
Carlos Eduardo Andrade Iatskiu <carlosiatskiu@MacBook-Pro-de-Carlos.local>
2022-08-28 20:11:42 -0300

    subindo projeto

---
+++ src/app/pages/detalhar/detalhar.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TouchSequence } from 'selenium-webdriver';
+import { Contato } from 'src/app/models/contato';
+
+@Component({
+  selector: 'app-detalhar',
+  templateUrl: './detalhar.page.html',
+  styleUrls: ['./detalhar.page.scss'],
+})
+export class DetalharPage implements OnInit {
+  contato: Contato;
+  nome: string;
+  telefone: number;
+  genero: string;
+  dataNascimento: string;
+  data: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    const nav = this.router.getCurrentNavigation();
+    this.contato = nav.extras.state.objeto;
+    this.data = new Date().toISOString();
+    this.nome = this.contato.nome;
+    this.telefone = this.contato.telefone;
+    this.genero = this.contato.genero;
+    this.dataNascimento = this.contato.data_nascimento
+  }
+
+  editar(){}
+  excluir(){}
+
+}
---
+++ src/app/services/contato.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Contato } from '../models/contato';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+  private _contatos : Contato[] = []
+
+  constructor() {
+    let contato = new Contato("Teste", 123,
+    "masculino", "1986-12-20");
+    this.inserir(contato);
+   }
+
+  public get contatos(): Contato[]{
+    return this._contatos;
+  }
+
+  public inserir(contato: Contato){
+    this._contatos.push(contato);
+  }
+}
